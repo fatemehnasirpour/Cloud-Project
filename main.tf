e2e9485d241f7c37941f917f8015c6293b1cfb88
fatemehnasirpour <94791299+fatemehnasirpour@users.noreply.github.com>
2025-03-07 17:31:28 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Create a VPC to launch our instances into
+resource "aws_vpc" "dev_vpc" {
+  cidr_block = "10.0.0.0/16"  
+  enable_dns_hostnames = true 
+  enable_dns_support = true
+  
+  tags       =  {
+    name     = "deham9"
+  }       
+}
+
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deham9"
+  }
+}
